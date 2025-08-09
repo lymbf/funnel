27f0adcf5d9482aeaed6d485a67ffff5a96577c8
lymbf <paw.czekaj@gmail.com>
2025-08-09 23:18:05 +0200

    parallel props sorted

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type {Metadata} from "next";
-import { Lato} from "next/font/google";
+import {Lato} from "next/font/google";
 import "./globals.css";
+import React from "react";
 
 const lato = Lato({
     variable: '--font-lato-sans',
-    weight:['100', '300', '400', '700', '900'],
-    subsets:['latin']
+    weight: ['100', '300', '400', '700', '900'],
+    subsets: ['latin']
 })
 
 export const metadata: Metadata = {
@@ -15,10 +16,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
                                        children,
-                                       header
+                                       header,
+                                       header2
                                    }: Readonly<{
     children: React.ReactNode;
     header: React.ReactNode;
+    header2: React.ReactNode;
 }>) {
     return (
         <html lang="en">
