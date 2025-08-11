59ac022a3610b05cc1c548ca345c242fe4848a67
lymbf <paw.czekaj@gmail.com>
2025-08-11 20:24:04 +0200

    hover:card:border effect

--- components/form/card.tsx
+++ components/form/card.tsx
@@ -10,7 +10,8 @@ export default function Card({active, children, onClick}: CardProps){
         <button
             type="button"
             onClick={onClick}
-            className={`group rounded-2xl border box-border p-4 md:p-5 text-left transition shadow-sm hover:shadow-md focus:outline-none
+            className={`group rounded-2xl border box-border p-4 md:p-5 text-left transition-all shadow-sm hover:shadow-md focus:outline-none
+            hover:border-[3px] hover:border-accent hover:bg-blue-100  duration-200
         ${active ? "border-[3px] border-accent bg-blue-100" : "border-accent bg-white hover:bg-gray-50"}
        w-full h-[70px] sm:w-[200px] sm:h-[240px]
         `}
