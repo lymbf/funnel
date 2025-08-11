0cf47f52a4ee8ce241377a0dedcea30962c5df02
lymbf <paw.czekaj@gmail.com>
2025-08-11 19:53:50 +0200

    logo expanded

--- app/@header/page.tsx
+++ app/@header/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
             className={cn('h-[90px] w-full flex items-center justify-center',
                 'drop-shadow-lg bg-white border-b-solid border-b-[1px] border-b-slate-100 ')}>
             <div className={'w-full max-w-[1200px] flex items-center justify-between px-4 sm:px-0'}>
-                <Image className={'h-[30px] sm:h-[50px] w-auto'} src={'/images/logo/logo_light_blue.svg'} alt={'logo'} width={733.5} height={137}
+                <Image className={'h-[30px] sm:h-[50px] w-auto'} src={'/images/logo/logo_light_blue_expanded.svg'} alt={'logo'} width={733.5} height={137}
                       />
                 <div className={'flex flex-row items-center'}>
                     <Phone size = {32} className={'w-[28px] sm:w-[40px] stroke-black mr-2 sm:mr-4'}/>
