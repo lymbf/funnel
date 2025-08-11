7e2d27e3b66583d4e0b4cf9045c7154487aa25f1
lymbf <paw.czekaj@gmail.com>
2025-08-11 23:57:22 +0200

    card hover color

--- components/form/card.tsx
+++ components/form/card.tsx
@@ -12,7 +12,7 @@ export default function Card({active, children, onClick}: CardProps){
             onClick={onClick}
             className={`group rounded-2xl border box-border p-4 md:p-5 text-left transition-all shadow-sm hover:shadow-md focus:outline-none
             hover:border-[3px] hover:border-accent duration-400
-        ${active ? "border-[3px] border-accent bg-green-100" : "border-accent bg-white hover:bg-gray-50"}
+        ${active ? "border-[3px] border-accent bg-[#B6E8E5]" : "border-accent bg-white hover:bg-gray-50"}
        w-full h-[70px] sm:w-[200px] sm:h-[240px]
         `}
         >
