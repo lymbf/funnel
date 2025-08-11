9ba3cd5784f6c5640f730df903e70fa00d8db45a
lymbf <paw.czekaj@gmail.com>
2025-08-11 23:50:25 +0200

    minor fix

--- components/form/card.tsx
+++ components/form/card.tsx
@@ -11,7 +11,7 @@ export default function Card({active, children, onClick}: CardProps){
             type="button"
             onClick={onClick}
             className={`group rounded-2xl border box-border p-4 md:p-5 text-left transition-all shadow-sm hover:shadow-md focus:outline-none
-            hover:border-[3px] hover:border-accent hover:bg-blue-100  duration-400
+            hover:border-[3px] hover:border-accent duration-400
         ${active ? "border-[3px] border-accent bg-green-100" : "border-accent bg-white hover:bg-gray-50"}
        w-full h-[70px] sm:w-[200px] sm:h-[240px]
         `}
