84792f00928bf681e5b9dea1c118520fc29c781c
lymbf <paw.czekaj@gmail.com>
2025-08-12 22:22:44 +0200

    types error fixed

--- components/GA/gtagHelper.ts
+++ components/GA/gtagHelper.ts
@@ -1,5 +1,12 @@
+declare global {
+    interface Window {
+        cloudinary: any;
+        gtag: (...args: any[]) => void;
+    }
+}
+
 export const pv = (GA_MEASUREMENT_ID:string, url:string)=>{
-    // @ts-ignore
+
     window.gtag('config', GA_MEASUREMENT_ID, {
         page_path: url
     })
