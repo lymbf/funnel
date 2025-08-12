158f043051c5a6ef7dcdf7bc89e265ff6395c093
lymbf <paw.czekaj@gmail.com>
2025-08-12 15:23:24 +0200

    icon:changed:door_window

--- components/svgs/door_window.tsx
+++ components/svgs/door_window.tsx
@@ -3,41 +3,18 @@ export default function Door_window({className}:{className?:string}){
     return (
         <svg className={className} id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 398.22 688.3">
             <g id="Icons">
-                <g>
-                    <rect x="7.11" y="647.75" width="384" height="29.44" className={'fill-accent'}/>
-                    <polygon
-                        points="7.11 647.75 7.11 159.11 391.11 159.11 391.11 647.75 359.11 647.75 359.11 206.78 39.11 206.78 39.11 647.75 7.11 647.75"
-                        className={'fill-accent'}/>
-                    <rect x="42.44" y="191.11" width="320" height="456.64" fill="#ebedf0"/>
-                    <path d="M391.11,7.11v152H7.11V7.11h384ZM359.11,127.11V39.11H39.11v88h320Z" className={'fill-accent'}/>
-                    <polygon points="359.11 39.11 359.11 127.11 199.11 127.11 199.11 119.11 199.11 39.11 359.11 39.11"
-                             fill="#fff"/>
-                    <polygon points="199.11 119.11 199.11 127.11 39.11 127.11 39.11 39.11 199.11 39.11 199.11 119.11"
-                             fill="#fff"/>
-                    <polygon
-                        points="199.11 39.11 359.11 39.11 359.11 127.11 199.11 127.11 39.11 127.11 39.11 39.11 199.11 39.11"
-                        fill="none" stroke="#303e58" strokeLinecap="round" strokeLinejoin="round" strokeWidth="12"/>
-                    <polygon
-                        points="391.11 681.19 391.11 220.57 391.11 7.11 7.11 7.11 7.11 220.57 7.11 681.19 391.11 681.19"
-                        fill="none" stroke="#303e58" strokeLinecap="round" strokeLinejoin="round"
-                        strokeWidth="14.22"/>
-                    <rect x="39.11" y="191.11" width="320" height="456.64" fill="none" stroke="#303e58"
-                          strokeLinecap="round" strokeLinejoin="round" strokeWidth="14.9"/>
-                    <line x1="7.11" y1="159.11" x2="391.11" y2="159.11" fill="none" stroke="#303e58"
+                <rect x="7.11" y="647.75" width="384" height="29.44" className='fill-accent'/>
+                <polygon
+                    points="7.11 647.75 7.11 4.86 391.11 4.86 391.11 647.75 359.11 647.75 359.11 67.58 39.11 67.58 39.11 647.75 7.11 647.75"
+                    className='fill-accent'/>
+                <rect x="42.44" y="40.86" width="320" height="606.89" fill="#ebedf0"/>
+                <polygon
+                    points="391.11 681.19 391.11 220.57 391.11 7.11 7.11 7.11 7.11 220.57 7.11 681.19 391.11 681.19"
+                    fill="none" stroke="#303e58" strokeLinecap="round" strokeLinejoin="round" strokeWidth="14.22"/>
+                <rect x="39.11" y="39.52" width="320" height="608.22" fill="none" stroke="#303e58"
+                      strokeLinecap="round" strokeLinejoin="round" strokeWidth="14.9"/>
+                <polyline points="303.11 270.3 319.11 270.3 319.11 326.3 303.11 326.3" fill="none" stroke="#303e58"
                           strokeLinecap="round" strokeLinejoin="round" strokeWidth="12"/>
-                    <line x1="199.11" y1="39.11" x2="199.11" y2="119.11" fill="none" stroke="#303e58"
-                          strokeLinecap="round" strokeLinejoin="round" strokeWidth="12"/>
-                    <polyline points="303.11 415.11 319.11 415.11 319.11 471.11 303.11 471.11" fill="none"
-                              stroke="#303e58" strokeLinecap="round" strokeLinejoin="round" strokeWidth="12"/>
-                    <line x1="79.11" y1="87.11" x2="95.11" y2="71.11" fill="none" stroke="#979faf"
-                          strokeLinecap="round" strokeLinejoin="round" strokeWidth="12"/>
-                    <line x1="111.11" y1="95.11" x2="119.11" y2="87.11" fill="none" stroke="#979faf"
-                          strokeLinecap="round" strokeLinejoin="round" strokeWidth="12"/>
-                    <line x1="239.11" y1="87.11" x2="255.11" y2="71.11" fill="none" stroke="#979faf"
-                          strokeLinecap="round" strokeLinejoin="round" strokeWidth="12"/>
-                    <line x1="271.11" y1="95.11" x2="279.11" y2="87.11" fill="none" stroke="#979faf"
-                          strokeLinecap="round" strokeLinejoin="round" strokeWidth="12"/>
-                </g>
             </g>
         </svg>
     )
