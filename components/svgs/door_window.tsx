a30ccde0bf99892ab2062471443da8f92c1654c5
lymbf <paw.czekaj@gmail.com>
2025-08-12 16:09:36 +0200

    icon:door:handle lowered

--- components/svgs/door_window.tsx
+++ components/svgs/door_window.tsx
@@ -3,17 +3,17 @@ export default function Door_window({className}:{className?:string}){
     return (
         <svg className={className} id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 398.22 688.3">
             <g id="Icons">
-                <rect x="7.11" y="647.75" width="384" height="29.44" className='fill-accent'/>
+                <rect x="7.11" y="647.75" width="384" height="29.44" className={'fill-accent'}/>
                 <polygon
                     points="7.11 647.75 7.11 4.86 391.11 4.86 391.11 647.75 359.11 647.75 359.11 67.58 39.11 67.58 39.11 647.75 7.11 647.75"
-                    className='fill-accent'/>
-                <rect x="42.44" y="40.86" width="320" height="606.89" fill="#ebedf0"/>
+                    className={'fill-accent'}/>
+                <rect x="39.11" y="31.86" width="320" height="606.89" fill="#ebedf0"/>
                 <polygon
                     points="391.11 681.19 391.11 220.57 391.11 7.11 7.11 7.11 7.11 220.57 7.11 681.19 391.11 681.19"
                     fill="none" stroke="#303e58" strokeLinecap="round" strokeLinejoin="round" strokeWidth="14.22"/>
                 <rect x="39.11" y="39.52" width="320" height="608.22" fill="none" stroke="#303e58"
                       strokeLinecap="round" strokeLinejoin="round" strokeWidth="14.9"/>
-                <polyline points="303.11 270.3 319.11 270.3 319.11 326.3 303.11 326.3" fill="none" stroke="#303e58"
+                <polyline points="299.78 307.3 315.78 307.3 315.78 363.3 299.78 363.3" fill="none" stroke="#303e58"
                           strokeLinecap="round" strokeLinejoin="round" strokeWidth="12"/>
             </g>
         </svg>
